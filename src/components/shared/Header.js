373a151f403719294881b0647ca7818cb0c6b797
hayatul-islam <hayatulemon340@gmail.com>
2023-12-26 11:14:55 +0600

    mobile menu responsive

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiShoppingCart } from "react-icons/fi";
-import { MdMenu, MdOutlineSearch } from "react-icons/md";
+import { MdMenu, MdOutlineClose, MdOutlineSearch } from "react-icons/md";
 import { RiUser3Line } from "react-icons/ri";
 import logo from "../../assets/img/logo.svg";
 
 const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   const menu = [
     "brands",
     "cameras",
@@ -16,11 +17,11 @@ const Header = () => {
   ];
 
   return (
-    <div className="bg-black">
+    <div className="bg-black relative z-50">
       <div className="container py-7 space-y-10">
         <div className="flex justify-between items-center">
           <div className="lg:hidden">
-            <MdMenu className="text-white" size={28} />
+            <MdMenu onClick={setToggleMenu} className="text-white" size={28} />
           </div>
           <img className="h-[18px] lg:h-[30px] " src={logo} alt="" />
           <div className="flex gap-4 items-center">
@@ -32,6 +33,24 @@ const Header = () => {
         <ul className="hidden lg:flex justify-between text-white items-center uppercase font-medium text-[18px]">
           {menu?.map((item, i) => (
             <li className="cursor-pointer hover:text-primary" to="">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* mobile menu  */}
+      <div>
+        {toggleMenu && <div className="fixed inset-0 bg-black bg-opacity-25" />}
+        <ul
+          className={`text-white fixed top-0 bg-black w-[70%] left-0 h-[100vh] p-4 space-y-2 duration-500 delay-100 ${
+            toggleMenu ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="flex justify-end">
+            <MdOutlineClose onClick={() => setToggleMenu(false)} size={24} />
+          </div>
+          {menu?.map((item, i) => (
+            <li className="cursor-pointer hover:text-primary uppercase" to="">
               {item}
             </li>
           ))}
